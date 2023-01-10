7f128eead12ce35add62b6a74509e43f6a94c2e1
Algis <87173458+Angmarec398@users.noreply.github.com>
2023-01-10 08:49:54 +0300

    Данные по токену бота и назначение администратора

---
+++ auth_2.py
@@ -0,0 +1,9 @@
+token = ""  # ID бота который выдает BotFather
+admin_list = []  # User ID администратора или администраторов
+
+address_1 = {'ip': '00.000.000.000:0000',
+             'for connect': {'login': 'xxxxxx', 'password': 'xxxxx'}}  # Подменные IP для запросов на сторонние сервисы
+address_2 = {'ip': '00.000.000.000:0000', 'for connect': {'login': 'xxxxxx', 'password': 'xxxxx'}}
+address_3 = {'ip': '00.000.000.000:0000', 'for connect': {'login': 'xxxxxx', 'password': 'xxxxx'}}
+
+proxy_list = [address_1, address_2, address_3]
